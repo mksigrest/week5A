deb77169ef3b1e2c54dc32093ad9e6469d8e50ee
mksigrest <mksigrest@gmail.com>
2024-09-24 09:02:37 -0400

    Added program Week5A

---
+++ week5A/week5A/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace week5A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int green = 22;
+            int blue = 23;
+            int sum1;
+            int sum2;
+
+            string peter = "hello there people";
+
+            double cha = 3.14;
+
+            bool light = true;
+
+            Console.WriteLine("Our Instructors Name is Nick");
+
+            sum1 = green + blue;
+            sum2 = green - blue;
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+        }
+    }
+}
